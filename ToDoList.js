f8d89428f427cd2e88a10398e89c9e2e46bc3382
Pranaybot <ppentaparthy@gmail.com>
2023-10-26 16:19:50 -0400

    modified files

--- ToDoList.js
+++ ToDoList.js
@@ -1,11 +1,13 @@
 const toDoForm = document.querySelector('.to-do-form');
 const toDoList = document.querySelector('.to-do-list');
+
 const savedErrands = localStorage.getItem("errands") ? JSON.parse(localStorage.getItem("errands")) : [];
 
 // retrieve from localStorage
 for (let errand of savedErrands) {
   let newErrand = document.createElement("li");
   let crossButton = document.createElement("button");
+  let completedTask;
   crossButton.innerHTML = '&#10005;';
 
   newErrand.innerText = errand.errand;
@@ -24,9 +26,9 @@ toDoForm.addEventListener("submit", function(event) {
     crossButton.innerHTML = '&#10005;';
 
     let newErrand = document.createElement("li");
-    newErrand.isCompleted = false;
     newErrand.innerText = document.getElementById("errand").value;
-    savedErrands.push({errandCompleted: false, errand: newErrand.innerText})
+    newErrand.isCompleted = false;
+    savedErrands.push({errandCompleted: newErrand.isCompleted, errand: newErrand.innerText})
     localStorage.setItem("errands", JSON.stringify(savedErrands));
     toDoList.appendChild(newErrand);
     newErrand.appendChild(crossButton);
@@ -37,11 +39,26 @@ toDoForm.addEventListener("submit", function(event) {
 toDoList.addEventListener("click", function(event) {
     const targetTag = event.target.tagName;
     if (targetTag === "BUTTON") {
-        event.target.parentNode.remove();
+        let child = event.target.parentNode;
+        let parent = child.parentNode;
+        imageIndex = console.log(Array.prototype.indexOf.call(parent.children, child));
+        savedErrands.splice(imageIndex, 1);
+        localStorage.setItem("errands", JSON.stringify(savedErrands));
+        child.remove();
     } else if (targetTag === "LI"){
         if (!event.target.isCompleted) {
             event.target.style.textDecoration = "line-through";
             event.target.isCompleted = true;
+            let child = event.target;
+            let parent = child.parentNode;
+            imageIndex = Array.prototype.indexOf.call(parent.children, child);
+
+            for(let i = 0; i < savedErrands.length; i++) {
+                if (i == imageIndex) {
+                    savedErrands[i].errandCompleted = true;
+                    localStorage.setItem("errands", JSON.stringify(savedErrands));
+                }
+            }
         }
     }
 });
